a0913c21dddfb31b1eb1ebd5f143e3dda01b3ffc
Kevin Gonzalez <kevingonzalezdw@gmail.com>
2024-03-30 10:06:14 -0600

    version 1

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			bg1 : '#292928',
+			bg2 : '#747473',
+			bg3 : '#4B4B4B',
+			bgDark : '#191919',
+			white1: '#d1d5db',
+			white2: '#d4d4d8',
+		},
+		extend: {},
+	},
+	plugins: [],
+}
